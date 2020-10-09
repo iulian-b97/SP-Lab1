408f09312f8adf14e2d9805089b203d9a6311d39
unknown <iulian.burghelea1997@gmail.com>
2020-10-09 20:03:16 +0300

    Initial Commit

---
+++ MyBook/src/com/company/Book.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Book
+{
+    public String book_name;
+
+    public Book(String book_name)
+    {
+        this.book_name = book_name;
+    }
+
+    public void createNewParagraph(String paragraph)
+    {
+        System.out.println(paragraph + " has been created.");
+    }
+    public void createNewImage(String image)
+    {
+        System.out.println(image + " has been created.");
+    }
+    public void createNewTable(String table)
+    {
+        System.out.println(table + " has been created.");
+    }
+    public void print()
+    {
+        System.out.println(book_name);
+    }
+}
